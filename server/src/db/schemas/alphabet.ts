6ce8e90a0aa5589752793e403fa6d5981ff0b801
darianmorat <yojhandariantoledomora@gmail.com>
2025-08-12 15:00:31 -0500

    feat(db): add imageUrl column to alphabet table

--- server/src/db/schemas/alphabet.ts
+++ server/src/db/schemas/alphabet.ts
@@ -10,6 +10,7 @@ export const alphabet = pgTable("alphabet", {
    order: integer("order").notNull(),
    letter: char("letter", { length: 1 }).notNull(),
    videoUrl: varchar("video_url").notNull(),
+   imageUrl: varchar("image_url").notNull(),
 });
 
 export const alphabetProgress = pgTable("alphabet_progress", {
